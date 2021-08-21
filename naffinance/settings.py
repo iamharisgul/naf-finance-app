89a26e1eb14af1f56bec9e1b10510910da8c4038
Haris Gul <harisgul2999@gmail.com>
2021-08-22 02:14:52 +0500

    SSL for heroku

--- naffinance/settings.py
+++ naffinance/settings.py
@@ -158,3 +158,8 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if os.getcwd() == '/app':
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    DEBUG = False
