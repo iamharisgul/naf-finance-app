cd6bc4fff4c249517075add64dca724f9d29de11
Haris Gul <harisgul2999@gmail.com>
2021-08-23 10:54:39 +0500

    errors

--- naffinance/settings.py
+++ naffinance/settings.py
@@ -155,6 +155,9 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 django_heroku.settings(locals())
 
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
