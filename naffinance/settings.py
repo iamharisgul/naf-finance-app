f3ae2dfa0b0fd6c0bce5ae45c55a705b6f7897bd
Haris Gul <harisgul2999@gmail.com>
2021-08-23 11:38:18 +0500

    bug fixing

--- naffinance/settings.py
+++ naffinance/settings.py
@@ -179,4 +179,4 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = 'ap-south-1'
-AWS_QUERYSTRING_AUTH = False
+
